13d07d7425142ac5683955871938a9c3f994be60
Max Wofford <max@maxwofford.com>
2024-04-22 14:13:58 -0400

    Fix key in update

--- api/subscribe.js
+++ api/subscribe.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
     }
 
     await loops.updateContact(email, {
-      subscribeField: Date.now()
+      [subscribeField]: Date.now()
     })
 
     res.status(200).json({ ok: true })
