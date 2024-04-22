15bf0936c5c5cd3da5c10111a00cf7d0921bf1de
Max Wofford <max@maxwofford.com>
2024-04-22 14:13:38 -0400

    Update subscribed fields to not be array

--- api/subscribe.js
+++ api/subscribe.js
@@ -2,7 +2,7 @@ import { LoopsClient } from "loops"
 
 export default async function handler(req, res) {
   try {
-    const { email, subscribeFields } = req.body
+    const { email, subscribeField } = req.body
 
     const loops = await new LoopsClient(process.env.LOOPS_API_KEY);
 
@@ -10,16 +10,14 @@ export default async function handler(req, res) {
 
     if (foundContacts.length == 0) { // if the contact isn't already in the DB
       await loops.createContact(email, {
-        source: `API Email Subscribe - ${subscribeFields.join(",")}`,
+        source: `API Email Subscribe - ${subscribeField}`,
         userGroup: "Hack Clubber",
       })
     }
 
-    const fieldsToUpdate = []
-    subscribeFields.forEach(field => {
-      fieldsToUpdate[field] = Date.now()
+    await loops.updateContact(email, {
+      subscribeField: Date.now()
     })
-    await loops.updateContact(email, fieldsToUpdate)
 
     res.status(200).json({ ok: true })
   } catch (error) {
